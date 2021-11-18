953c5f055fb694e504a8b253548c0aa3a4d7285e
IAN BRANDON PALACIOS <IPALACIOSX@uanl.edu.mx>
2021-11-17 19:49:34 -0600

    cambio

---
+++ AAVDarkSouls-Pia-master/Forms/Client.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AAVD.Forms
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
